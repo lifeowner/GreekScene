bffe6dd10bccc5fbcaf03e5ab74ae3b7fd4b3f96
lifeowner <lifeowner@windowslive.com>
2019-08-04 01:25:52 +0300

    Example Project
    Ο κώδικας απο ένα τέστ Project έτοιμο σε C# γραμμένο για να δείτε πως λειτουργεί.

---
+++ Source Code/example_test/example_test/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GreekScene_API_V2;
+
+namespace example_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GreekScene_API_V2.sign_in_form RequiredSignIn = new GreekScene_API_V2.sign_in_form();
+            RequiredSignIn.ShowDialog();
+        }
+    }
+}
